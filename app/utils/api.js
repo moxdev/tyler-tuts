dc7bee80ed1f0e741a6074750d6b305b3dcd76ca
moxdev <moxdev1@gmail.com>
2017-11-15 14:21:01 -0500

    holy crap batman

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,58 @@
 import axios from 'axios';
 
+function getProfile(username) {
+  return axios.get('https://api.github.com/' + username + params).then(function(user) {
+    return user.data;
+  });
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/' + username + '/repos' + params + '&per_page=100');
+}
+
+function getStartCount(repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore(profile, repos) {
+  const followers = profile.followers;
+  const totalStars = getStartCount(repos);
+
+  return followers * 3 + totalStars;
+}
+
+function handleError(error) {
+  console.log(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([getProfile(player), getRepos(player)]).then(function(data) {
+    const profile = data[0];
+    const repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    };
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort(function(a, b) {
+    return b.score - a.score;
+  });
+}
+
 module.exports = {
+  battle: function(players) {
+    return axios
+      .all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError);
+  },
   fetchPopularRepos: function(language) {
     var encodedURI = window.encodeURI(
       'https://api.github.com/search/repositories?q=stars:>1+language:' +
